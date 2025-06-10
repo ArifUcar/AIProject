78c1cebdb543853afd1ca37fbf9f9ca3b382e0da
Arif Uçar <102770456+LHOST1@users.noreply.github.com>
2025-06-10 11:50:44 +0300

    guncel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { LoginPageComponent } from './Modules/Auth/login-page/login-page.compone
 import { RegisterPageComponent } from './Modules/Auth/register-page/register-page.component';
 import { ChatPageComponent } from './Modules/User/chat-page/chat-page.component';
 import { AuthGuard } from './Core/Guards/auth.guard';
-import { ChoosePlanPageComponent } from './Modules/User/choose-plan-page/choose-plan-page.component';
 
 export const routes: Routes = [
     {
@@ -22,11 +21,6 @@ export const routes: Routes = [
     {
         path: 'chat',
         component: ChatPageComponent,
-        canActivate: [AuthGuard]
-    },
-    {
-        path: 'choose-plan',
-        component: ChoosePlanPageComponent,
         canActivate: [AuthGuard]
     },
     {
